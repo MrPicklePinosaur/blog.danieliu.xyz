a7908c91315c3bae19886ab2319de6d87793b585
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-12-22 13:21:57 -0500

    first post

---
+++ include.js
@@ -0,0 +1,10 @@
+document.getElementById("header-banner").innerHTML = `
+
+        <a class="logo" href="blog.danieliu.xyz/rolling.html"><img src="blog.danieliu.xyz/static/pinolog_banner.png"/></a>
+        <div class="navcontainer">
+            <a class="navitem" href="blog.danieliu.xyz/rss.xml">rss</a>
+        </div>
+        <div class="navcontainer">
+            <a class="navitem" href="blog.danieliu.xyz/blogindex.html">index</a>
+        </div>
+`
