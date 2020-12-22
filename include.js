1b4a27a4f13ccd2ce99bb7ed852ea02fc3ef4ebb
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-12-22 13:39:11 -0500

    url fixes

--- include.js
+++ include.js
@@ -1,10 +1,12 @@
+var url = "https://blog.danieliu.xyz";
+
 document.getElementById("header-banner").innerHTML = `
 
-        <a class="logo" href="blog.danieliu.xyz/rolling.html"><img src="blog.danieliu.xyz/static/pinolog_banner.png"/></a>
+        <a class="logo" href="${url}/rolling.html"><img src="${url}/static/pinolog_banner.png"/></a>
         <div class="navcontainer">
-            <a class="navitem" href="blog.danieliu.xyz/rss.xml">rss</a>
+            <a class="navitem" href="${url}/rss.xml">rss</a>
         </div>
         <div class="navcontainer">
-            <a class="navitem" href="blog.danieliu.xyz/blogindex.html">index</a>
+            <a class="navitem" href="${url}/blogindex.html">index</a>
         </div>
 `
